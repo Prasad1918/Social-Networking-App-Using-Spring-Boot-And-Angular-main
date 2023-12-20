40072f8abcbed17d1a917e81c573ffd78371a02e
Prasad1918 <prasadrelangi18>
2023-12-20 20:55:56 +0530

    fronend commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	apiUrl: 'http://localhost:8080/api/v1',
+	apiUrl: 'http://localhost:8090/api/v1',
 	defaultProfilePhotoUrl: 'http://localhost:4200/assets/images/avatar.jpg',
 	defaultCoverPhotoUrl: 'http://localhost:4200/assets/images/cover.jpg'
 };
